e7d701c8debdcd36acdc695048e400e8155847df
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2024-09-13 13:58:48 +0200

    förbättre

--- Program.cs
+++ Program.cs
@@ -2,24 +2,24 @@
 int rand = rng.Next(1, 101); 
 int guess = 0;
 int P_turns = 0;
-bool start1 = false;
+bool start = false;
 Console.WriteLine("To start enter !");
-while(!start1) {
-    string start2 = Console.ReadLine();
-    if (start2 == "!"){
-        start1 = true;    
+while(!start) {
+    string startup = Console.ReadLine();
+    if (startup == "!"){
+        start = true;    
     } 
-}while (start1) {
-    P_turns++;
+}while (start) {
     if (P_turns == 0) {
         Console.WriteLine("Guess the right number between 1 and 100 to win.");
     }
+    P_turns++;
     Console.Write("Guess" + P_turns + ": ");
     guess = Convert.ToInt32(Console.ReadLine());
     if (guess == rand) {
         Console.WriteLine("You win!!!");
         Console.WriteLine("It took " + P_turns + " turns for you to get it.");
-        start1 = false;
+        start = false;
     } 
     /*else if (guess == 111) {
         Console.WriteLine(rand);
